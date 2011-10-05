a53cd6419e86e2488fb4ada213ff97a859ded305
Burak AKIN <burak.akin@gmail.com>
2011-10-05 22:58:13 +0300

    fixed assets2

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Grupblog
 
     # Enable the asset pipeline
     config.assets.enabled = false
-
+	config.assets.compile = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
